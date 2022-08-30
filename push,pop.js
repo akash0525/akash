9b8f25066ae548ad2d52f7ccb2a40f290ba935ad
akash <108937666+akash0525@users.noreply.github.com>
2022-08-30 07:23:18 +0530

    Add files via upload

---
+++ push,pop.js
@@ -0,0 +1,6 @@
+let largeCountries = ["Tuvalu","India","USA","Indonesia","Monaco"]
+largeCountries.pop()
+largeCountries.push("Pakistan")
+largeCountries.shift()
+largeCountries.unshift("China")
+console.log(largeCountries)
